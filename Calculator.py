54dbd28ac97e87f81b0244a2f17216424af26a85
matchagreenteacake <40091700+matchagreenteacake@users.noreply.github.com>
2020-08-09 01:55:37 -0400

    Update Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+import math
+
+# A program that's probably going to save you $1 on your next soda bulk purchase
+# Opinion: Fluid Ounce / ML per $ is a better measurement than the other way round and much more comparable for a human
+
+FIOZ_TO_ML: float = 29.574
+
+floz = 16
+unit = 8
+price = 1
+
+txt =(
+f"{unit} container(s) sold at ${price} each containing {floz} fluid ounces mean: \n" \
+f"Fl Oz /$: {floz * unit} \n" \
+f"ml/$: {floz * unit * FLOZ_TO_ML}")
+
+
+print(txt)
+
+
